49f4eb04a8dec487bb06e741685a1151cd1b5faf
Franck Richaud <richaudfranck93@gmail.com>
2020-12-16 17:50:05 +0100

    Adding Home Route

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ app.get('/', (req, res) => {
   res.send('Hello from 4myson.fr!')
 })
 
+app.get('/Home', (req, res) => {
+  res.send('Hello from Home of 4myson.fr!')
+})
+
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
